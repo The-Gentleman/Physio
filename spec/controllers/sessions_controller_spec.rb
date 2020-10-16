4e945f3dfb298d08d7053d05cce5f36c421ad109
The-Gentleman <zraja4402@gmail.com>
2020-10-16 19:33:28 -0400

    pseudo coded tests for sessions controller

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController do 
+    # I want the controller to have a new action
+    # I want this action to instantiate a new user 
+    
+# I want this controller to have a create action
+    # I want this action to find the user 
+    # If the user exists and the password is authenticated
+    # I want this action to log the user in
+    # I want this action to redirect to the users show page
+    # if it isnt
+    # display a message
+    # and redirect to sessions/new
+
+# I want this controller to have a login action 
+
+# I want this controller to have a logout action
+
+end 
