7775f7cdab4ebfa18b1e79023de0382d05daedbc
The-Gentleman <zraja4402@gmail.com>
2020-10-23 19:28:46 -0400

    added divs and UX design

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,11 +3,6 @@ class Office < ApplicationRecord
     has_many :users, through: :patients
     validates :state, presence: true, uniqueness: true
     validates :city, presence: true, uniqueness: true
-    # scope method: Which office(s) has the most patients?
-    # query the office table to find which office 
-    # has the most patient ids
-    # binding.pry
-   scope :most_frequented_office, -> {order(patient_id: :ASC).limit(2)}
+   scope :most_frequented_office, -> {order(patient_id: :ASC).limit(3)}
    scope :alpha, -> {order 'offices.state ASC'}
-
  end
